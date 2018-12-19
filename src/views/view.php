14b218f675a40be990e756bc6d25da9d5d49a687
Dmitriy Sabirov <web8dew@yandex.ru>
2018-12-19 10:50:51 +0300

    test to add cropper with default settings

--- src/views/view.php
+++ src/views/view.php
@@ -7,4 +7,39 @@
  * @since       19.12.18
  */
 
+use sabirov\cropper\CropperAsset;
+
+/**
+ * @var $this \yii\web\View
+ */
+
+CropperAsset::register($this);
+
 echo "просто кроппер";
+?>
+<div>
+    <img id="image" src="picture.jpg">
+</div>
+
+<?php
+$initializeCropper = <<<JS
+var image = jQuery('#image');
+
+image.cropper({
+  aspectRatio: 16 / 9,
+  crop: function(event) {
+    console.log(event.detail.x);
+    console.log(event.detail.y);
+    console.log(event.detail.width);
+    console.log(event.detail.height);
+    console.log(event.detail.rotate);
+    console.log(event.detail.scaleX);
+    console.log(event.detail.scaleY);
+  }
+});
+
+// Get the Cropper.js instance after initialized
+var cropper = image.data('cropper');
+JS;
+
+$this->registerJs($initializeCropper);
