04de2dbc902b71509d3420d2a8427d73f6b409c3
Dmitriy Sabirov <web8dew@yandex.ru>
2018-12-19 10:36:37 +0300

    create empty views

---
+++ src/views/modal.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @author      Dmitriy Sabirov <web8dew@yandex.ru>
+ * @copyright   Dmitriy Sabirov 19.12.18
+ * @license     MIT
+ * @license     https://opensource.org/licenses/MIT
+ * @since       19.12.18
+ */
---
+++ src/views/view.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author      Dmitriy Sabirov <web8dew@yandex.ru>
+ * @copyright   Dmitriy Sabirov 19.12.18
+ * @license     MIT
+ * @license     https://opensource.org/licenses/MIT
+ * @since       19.12.18
+ */
+
+echo "просто кроппер";
